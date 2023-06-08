94edcae0cf960e16e3e02f49c5279c41431c5637
Akshat-Saxena333 <85073887+Akshat-Saxena333@users.noreply.github.com>
2023-06-09 00:20:03 +0530

    2.7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,23 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/header';
+import Footer from './components/footer';
+
+
+
 const App = () =>{
   return (
+    <>
+
+    <Header />
+    <main className='py-3'>
+      <Container>
     <h1>Welcome to shoptronics</h1>
-  )
-}
-export default App
+
+      </Container>
+    </main>
+    <Footer />
+
+    </>
+  );
+};
+export default App;
