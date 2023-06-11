7e11fac231c095fc98ad0436282e96a108de08fa
Akshat-Saxena333 <85073887+Akshat-Saxena333@users.noreply.github.com>
2023-06-12 01:39:57 +0530

    24

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-import products from './data/products.js';
+
+import productRoutes from './routes/productRoutes.js';
 const port= process.env.PORT || 5000;
 import connectDB from './config/db.js';
 
@@ -15,15 +16,7 @@ app.get('/',(req,res) =>{
 });
 
 
-app.get('/api/products', (req,res) =>{
-    res.json(products);
-});
-
-
-app.get('/api/products/:id', (req,res) =>{
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 
 app.listen(port,() => console.log(`Server running on port ${port}`));
