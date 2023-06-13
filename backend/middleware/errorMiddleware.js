3c62ffa43516bae5e1e8bc1a824186d23c40bf15
Akshat-Saxena333 <85073887+Akshat-Saxena333@users.noreply.github.com>
2023-06-13 23:31:12 +0530

    25

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,31 @@
+// this function will be executed when no other middleware has handled the request 
+
+const notFound = (req,res,next) =>{
+    const error = new Error (`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+
+}
+
+
+// to overwrite the default express handler, we need this function
+
+const errorHandler = (err, req, res,next) =>{
+    let statusCode= res.statusCode === 200 ? 500 : res.statusCode;
+    let message = err.message
+
+
+    // check for Mongoose bad ObjectId
+
+    if(err.name === 'CastError' && err.kind=== 'ObjectId'){
+        message= `Resource not found`;
+        statusCode = 404;
+    }
+
+    res.status(statusCode).json({
+        message,
+        stack: process.env.NODE_ENV === 'production' ? null: err.stack,
+    });
+};
+
+export { notFound, errorHandler};
