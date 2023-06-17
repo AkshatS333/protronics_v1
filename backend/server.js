4e9ff02b4bb6aafcfdb5da7491ddb945c2fc6aca
Akshat-Saxena333 <85073887+Akshat-Saxena333@users.noreply.github.com>
2023-06-17 18:05:25 +0530

    40

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,11 @@ connectDB();   // connect to MongoDB
 
 const app =express();
 
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get('/',(req,res) =>{
     res.send('API is running...');
 });
