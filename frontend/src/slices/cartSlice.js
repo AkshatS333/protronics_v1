4f471c481d21c4ad34acdc1bd76b6eaf848ef13a
Akshat-Saxena333 <85073887+Akshat-Saxena333@users.noreply.github.com>
2023-06-17 11:38:23 +0530

    35

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { updateCart } from '../utils/cartUtils';
 
 
 const initialState = localStorage.getItem('cart')
@@ -6,9 +7,7 @@ const initialState = localStorage.getItem('cart')
     : { cartItems: []};
 
 
-const addDecimals= (num) =>{
-    return (Math.round(num*100)/100).toFixed(2);
-}
+
 
 
 const cartSlice = createSlice({
@@ -29,23 +28,10 @@ const cartSlice = createSlice({
                 state.cartItems= [...state.cartItems,item]
             }
 
-            // calculate items price
-            state.itemsPrice = addDecimals(state.cartItems.reduce((acc,item) => acc + item.price * item.qty , 0));
 
-            // Calculate shipping price (If order is over $100 then free, else $10 shipping)
-            state.shippingPrice = addDecimals(state.itemsPrice> 100 ? 0: 10);
+            return updateCart(state);
 
-            // Calculate tax price (15% tax)
-            state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
 
-            // Calculate total price
-            state.totalPrice= (
-                Number(state.itemsPrice)+
-                Number(state.shippingPrice)+
-                Number(state.taxPrice)
-            ).toFixed(2)
-
-            localStorage.setItem('cart',JSON.stringify(state));
         },
     },
 });
